d4b3987e45401395625bb124888669f64da0392f
Adnan Manzoor <adnan904@gmail.com>
2020-06-22 14:47:17 +0200

    Adding abilene 4in-varying-cap networks

---
+++ scripts/run_parallel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# (un)comment the Algorithm you want to use
+#algo='rs'
+#algo='lb'
+algo='sp'
+
+# use GNU parallel to run multiple repetitions and scenarios in parallel
+# run from project root! (where Readme is)
+parallel --bar $algo ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "200" ::: "--seed" :::: scripts/30seeds.txt
