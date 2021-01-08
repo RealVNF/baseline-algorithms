4e2698eed06a15e15f5d386c2ff35d57615408c5
Stefan Schneider <stefan.schneider@upb.de>
2021-01-08 11:22:35 +0100

    fix run script

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -7,4 +7,6 @@
 
 # use GNU parallel to run multiple repetitions and scenarios in parallel
 # run from project root! (where Readme is)
-parallel --bar :::: scripts/algo.txt ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "200" ::: "--seed" :::: scripts/30seeds.txt
+
+# DO NOT try to parallelize running sp and lb; the files are saved to the same place and hard (impossible?) to distinguish; run first sp, then lb
+parallel --bar 'sp' ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "200" ::: "--seed" :::: scripts/30seeds.txt
