73ffe126362bc10e8b6a29a032906f3a866a1164
Stefan Schneider <stefan.schneider@upb.de>
2021-01-08 10:19:45 +0100

    prepare inputs

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -3,8 +3,8 @@
 # (un)comment the Algorithm you want to use
 #algo='rs'
 #algo='lb'
-algo='sp'
+#algo='sp'
 
 # use GNU parallel to run multiple repetitions and scenarios in parallel
 # run from project root! (where Readme is)
-parallel --bar $algo ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "1000" ::: "--seed" :::: scripts/30seeds.txt
+parallel --bar :::: scripts/algo.txt ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "1000" ::: "--seed" :::: scripts/30seeds.txt
